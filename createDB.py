a6f107bdb5a5348ba52bda2d54006de0886092a4
Lucas Kearns <lucasrkearns@gmail.com>
2022-04-11 23:36:52 -0400

    Added the card info table

--- createDB.py
+++ createDB.py
@@ -11,7 +11,18 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS pokemonInfo (
                     "dexNum" INTEGER NOT NULL,
                     "pokemonName" TEXT NOT NULL,
                     PRIMARY KEY("dexNum")
-               )''')
+                )''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS cardInfo (
+                    "dexNum" INTEGER NOT NULL,
+                    "seriesNum" TEXT NOT NULL,
+                    "amount" INTEGER NOT NULL,
+                    "isFullArt" INEGER NOT NULL,
+                    "isFoil" INTEGER NOT NULL,
+                    "isReverseFoil" INTEGER NOT NULL,
+                    "notes" TEXT,
+                    FOREIGN KEY ("dexNum") REFERENCES pokemonInfo("dexNum")
+                 )''')
 
 # Save (commit) changes
 connection.commit()
