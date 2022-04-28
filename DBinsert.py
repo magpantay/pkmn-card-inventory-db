af877c285bf140bb92d073f47aa803f8dca07127
Kyran Magpantay <contact@kyran.xyz>
2022-04-27 19:55:25 -0700

    Added check to see whether Pokemon/Dex exists in DB, code cleanup

--- DBinsert.py
+++ DBinsert.py
@@ -12,38 +12,46 @@ import sqlite3
 
 db_filename = 'pkmn_cards.db'
 
+#%% Functions
+
+def getInput():
+    # gets entry input from console
+    dex = input('Pokedex Number: ')
+    mon = input('Pokemon Name: ')
+    return int(dex), mon
+
+
+def insertItem(item, connection):
+    cursor = connection.cursor()
+
+    existCheckQuery = '''SELECT *
+                         FROM pokemonInfo
+                         WHERE pokemonInfo.dexNum = ?
+                         OR lower(pokemonInfo.pokemonName) = lower(?)'''
+
+    cursor.execute(existCheckQuery, item)
+
+    existCheckResults = cursor.fetchall()
+
+    if (len(existCheckResults) > 0):
+        print("One or more inputs already exists in the database: ")
+        for row in existCheckResults:
+            print(row)
+    else:
+        sqlInsert = '''INSERT INTO pokemonInfo(dexNum, pokemonName)
+                       VALUES(?, ?)'''
+
+        cursor.execute(sqlInsert, item)
+        # command that adds data to the table
+
+        connection.commit()
 
 #%% Main
 
 def main():
     connection = sqlite3.connect(db_filename)
-
     entry = getInput()
-
     insertItem(entry, connection)
-
-
-#%% Functions
-
-def getInput():
-    #gets entry input from console
-
-    dex = input('Enter dex num being added:')
-    mon = input('Enter Pokemon Name:')
-    return int(dex), mon
-
-
-def insertItem(item, connection):
-
-    sqlInsert = '''INSERT INTO pokemonInfo(dexNum, pokemonName)
-             VALUES(?,?) '''
-
-    cursor = connection.cursor()
-    cursor.execute(sqlInsert, item)
-    #command that adds data to the table
-
-    connection.commit()
-
 
 #%% Root
 
