a291d779638e658acedf4ed9dafb9b4887ffe95d
Kyran Magpantay <contact@kyran.xyz>
2022-08-20 20:59:17 -0700

    Refactored dbQuery.py to take advantage of pkmnHelperModule

--- DBquery.py
+++ DBquery.py
@@ -6,86 +6,53 @@ Make sure DB exists by running createDB.py before using this
 
 #%% Imports and Vars
 
-import sqlite3
-
-dbFilename = 'pkmn_cards.db'
+import pkmnHelperModule
 
 #%% Functions
-# Get user input of the Pokemon name
+# Get user input of the Pokemon name or Pokedex number
 def getInput():
     userInput = input('Pokemon Name or Dex Number: ')
-    return userInput
 
-# Try to convert the value into an int, if it's able to then return true
-def isInt(userInput):
-    try:
-        int(userInput)
-        return bool(1)
-    except ValueError:
-        return bool(0)
+    if pkmnHelperModule.isInt(userInput):
+        pkmnHelperModule.validateDexNum(int(userInput))
+        return int(userInput) # if it passes the int check and validation, return the number value
+    else:
+        return pkmnHelperModule.nameToDexNum(userInput) # else convert it to a dex number and return the dex number
     
 # Process and potentially insert the entry received from the user
 def queryDB(connection, userInput):
     cursor = connection.cursor()
 
-    query = ""
-    queryParams = ()
-
-    if (isInt(userInput)):
-        query = ''' 
-                    SELECT pokemonInfo.dexNum, 
-                           pokemonInfo.pokemonName, 
-                            cardInfo.seriesNum, 
-                            cardInfo.amount, 
-                            CASE cardInfo.isFullArt
-                                WHEN 1 THEN "Yes"
-                                ELSE "No"
-                            END AS isFullArt,
-                            CASE cardInfo.isFoil
-                                WHEN 1 THEN "Yes"
-                                ELSE "No"
-                            END AS isFoil,
-                            CASE cardInfo.isReverseFoil
-                                WHEN 1 THEN "Yes"
-                                ELSE "No"
-                            END AS isReverseFoil, 
-                            cardInfo.notes
-                    FROM pokemonInfo
-                    LEFT JOIN cardInfo ON pokemonInfo.dexNum = cardInfo.dexNum
-                    WHERE pokemonInfo.dexNum = ?
-                '''
-        queryParams = (userInput,)
-    else:
-        query = '''
-                    SELECT  pokemonInfo.dexNum, 
-                            pokemonInfo.pokemonName, 
-                            cardInfo.seriesNum, 
-                            cardInfo.amount, 
-                            CASE cardInfo.isFullArt
-                                WHEN 1 THEN "Yes"
-                                ELSE "No"
-                            END AS isFullArt,
-                            CASE cardInfo.isFoil
-                                WHEN 1 THEN "Yes"
-                                ELSE "No"
-                            END AS isFoil,
-                            CASE cardInfo.isReverseFoil
-                                WHEN 1 THEN "Yes"
-                                ELSE "No"
-                            END AS isReverseFoil, 
-                            cardInfo.notes
-                    FROM pokemonInfo
-                    LEFT JOIN cardInfo ON pokemonInfo.dexNum = cardInfo.dexNum
-                    WHERE lower(pokemonInfo.pokemonName) like ?
-                '''
-        # For wildcards, need to put it as a param rather than in the query itself
-        queryParams = ('%' + userInput.lower() + '%',)
+    query = ''' 
+                SELECT  pokemonInfo.dexNum, 
+                        pokemonInfo.pokemonName, 
+                        cardInfo.seriesNum, 
+                        cardInfo.amount, 
+                        CASE cardInfo.isFullArt
+                            WHEN 1 THEN "Yes"
+                            ELSE "No"
+                        END AS isFullArt,
+                        CASE cardInfo.isFoil
+                            WHEN 1 THEN "Yes"
+                            ELSE "No"
+                        END AS isFoil,
+                        CASE cardInfo.isReverseFoil
+                            WHEN 1 THEN "Yes"
+                            ELSE "No"
+                        END AS isReverseFoil, 
+                        cardInfo.notes
+                FROM pokemonInfo
+                LEFT JOIN cardInfo ON pokemonInfo.dexNum = cardInfo.dexNum
+                WHERE pokemonInfo.dexNum = ?
+            '''
+    queryParams = (userInput,)
     
     cursor.execute(query, queryParams)
     queryResult = cursor.fetchall()
  
     if (len(queryResult) == 0):
-        print("No matches found.")
+        print("ERROR: No matches found.")
+        exit()
     else:
         userFriendlyFieldNames = ('Pokedex Number', 'Pokemon Name', 'Series', 'Amount', 'Is Full Art?', 'Is Foil?', 'Is Reverse Foil?', 'Notes')
         print ('-' * 40)
@@ -100,7 +67,7 @@ def queryDB(connection, userInput):
 #%% Main
 
 def main():
-    connection = sqlite3.connect(dbFilename)
+    connection = pkmnHelperModule.dbConnect()
     userInput = getInput()
     queryDB(connection, userInput)
     connection.close()
